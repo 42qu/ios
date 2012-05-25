50dc83693f0f6dc4c5c4250f7e364b52ae808141
Alex Rezit <AlexRezit@seymourdev.com>
2012-05-25 16:55:17 +0800

    Change structure of user phone and mail.

--- thrift/type.thrift
+++ thrift/type.thrift
@@ -4,7 +4,6 @@ typedef i64 UserID
 // ----- User Link -----
 
 typedef i64 LinkID
-
 enum {
   UserLinkTypeUnknown = 0,
   UserLinkType42qu,
@@ -18,10 +17,38 @@ struct UserLink {
   3:  required  string rid
 }
 
-struct UserPrivateInfo {
-  1:  required  string email,
-  2:  required  string phone,
-  3:  required  string wechat
+typedef i64 PhoneID
+enum {
+  UserPhoneTypeUnknown = 0,
+  UserPhoneTypePublic,
+  UserPhoneTypeMobile,
+  UserPhoneTypeHome,
+  UserPhoneTypeBusiness,
+  UserPhoneTypeFax,
+  UserPhoneTypeCustom
+} typedef i64 UserPhoneType
+
+struct UserPhone {
+  1:  required  PhoneID id,
+  2:  required  UserPhoneType cid,
+  3:  required  string rid,
+  4:  optional  string customType
+}
+
+typedef i64 MailID
+enum {
+  UserMailTypeUnknown = 0,
+  UserMailTypePublic,
+  UserMailTypeHome,
+  UserMailTypeBusiness,
+  UserMailTypeCustom
+} typedef i64 UserMailType
+
+struct UserMail {
+  1:  required  MailID id,
+  2:  required  UserMailType cid,
+  3:  required  string rid,
+  4:  optional  string customType
 }
 
 struct UserInfo {
@@ -29,6 +56,7 @@ struct UserInfo {
   2:  required  string nickname,
   3:  required  string introduction,
   4:  required  string picture,
-  5:  required  set<UserLink> snsAccounts,
-  6:  optional  UserPrivateInfo privateInfo
+  5:  required  list<UserLink> userLinkList,
+  6:  required  list<UserPhone> userPhoneList,
+  7:  required  list<UserMail> userMailList
 }
