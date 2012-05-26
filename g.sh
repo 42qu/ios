e63c5324d2194c5fc69a1c6d20e2c8b59b9103b4
范煜 <fy0748@gmail.com>
2012-05-26 11:25:30 +0800

    fix

--- g.sh
+++ g.sh
@@ -2,7 +2,7 @@
 files=$(ls ./thrift | grep '.thrift$')
 for file in $files
 do
-    thrift --gen py  -out service/. thrift/$file
+    thrift --gen py  -out utils/. thrift/$file
     thrift --gen cocoa -out cocoa/. thrift/$file
     echo "$file"
 done
