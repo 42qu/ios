ae1c7d912a263cd514b29997c9f90f9dbb709f8b
范煜 <fy0748@gmail.com>
2012-05-27 15:38:45 +0800

    fix

--- server/ctrl/ios.py
+++ server/ctrl/ios.py
@@ -2,32 +2,43 @@
 import _env
 #from utils.type.ttypes import 
 #from server.model.ios import t
-import server.model.ios as m
+import server.model.user as u
 import logging
 
 class Handler(object):
-    def login(self,auth):
-        return m.login(auth.user,auth.password,auth.clientKey,
-                auth.clientSecret)
+    def login_by_email(self,auth):
+        return u.login_by_email(auth.mail,auth.password,auth.client_id,
+                auth.client_secret)
 
-    def userInfo_get(self, accessToken, id):
-        return m.userinfo_get(accessToken, id)
-
-    def userInfo_set(self, accessToken, userInfo):
+    def login_by_oauth(self, client_key, client_secret):
         pass
 
-    def eventInfo_get(self, accessToken, id):
+    def login_by_oauth2(self, client_key, client_secret):
+        pass
+
+    def logout(self, acess_token):
         pass
     
-    def eventInfo_set(self, accessToken, eventInfo):
+    def user_info_get(self, access_token, id):
+        return u.user_info_get(access_token, id)
+
+    def user_info_set(self, access_token, user_info):
+        return u.user_info_set(access_token, user_info)
+
+    def task_get(self, access_token, id):
+        pass
+    
+    def task_new(self, access_token, task):
         pass
 
-    def eventPublish(self, accessToken):
+    def task_apply(self, access_token, task_id):
         pass
 
-    def eventApply(self, accessToken, id):
+    def task_reject(self, access_token, user_id):
+        pass
+
+    def task_accept(self, access_token, user_id):
         pass
 
 if __name__ == '__main__':
     pass
-
--- server/model/user.py
+++ server/model/user.py
@@ -2,17 +2,25 @@
 import _env
 from utils.type.ttypes import AuthResponse, AuthResponseStatus, UserInfo
 
-from model.user_mail import user_id_by_mail
+from model.user_mail import user_id_by_mail, mail_by_user_id
 from model.user_auth import mail_password_verify
 from model.zsite import Zsite
 #from model.api_user import json_info
 #from model.follow import follow_id_list_by_from_id, follow_id_list_by_to_id, follow_count_by_to_id, follow_count_by_from_id, follow_rm, follow_new
-from model.oauth2 import oauth_access_token_new, oauth_refresh_token_new, oauth_secret_verify
+from model.oauth2 import oauth_access_token_new, oauth_refresh_token_new, oauth_secret_verify, oauth_access_token_verify
 
-from array import array
+from model.ico import ico_url
 from time import time
 
-def login(mail, pw, client_id, client_secret):
+def access_token_verify(func):
+    def new(*args,**argkw):
+        if len(args)<1:
+            return func()
+        if oauth_access_token_verify(args[0]):
+            return func(*args,**argkw)
+    return new
+
+def login_by_email(mail, pw, client_id, client_secret):
     
     user_id = None
     if oauth_secret_verify(client_id, client_secret):
@@ -25,23 +33,44 @@ def login(mail, pw, client_id, client_secret):
                 name = user.name
                 
                 status = AuthResponseStatus.AUTH_SUCCESS
-                return AuthResponse({
-                    'status'        : status,
-                    'id'            : user_id,
-                    'name'          : name,
-                    'accessToken'   : access_token,
-                    'refresh_Token' : refresh_token,
-                    'expireDate'    : 87063,
-                })
+                return AuthResponse(
+                        status,
+                        user_id,
+                        name,
+                        access_token,
+                        refresh_token,
+                        87063
+                )
 
-def userinfo_get(accessToken, id):
-    user_id = user_id_by_mail(mail)
-    #return 
+@access_token_verify
+def user_info_get(access_token, id):
+    user = Zsite.mc_get(id)
+    if not user:
+        # TODO:ERROR 查无此人
+        return
+    name = user.name
+    intro = user.txt
+    pic = ico_url(id)
+    link = ['暂无']
+    phone = ['暂无']
+    mail = [mail_by_user_id(id)]
+    return UserInfo(id, name, intro, pic, link, phone, mail)
 
-def userinfo_set(accessToken, userInfo):
+def user_info_set(access_token, user_info):
     pass
 
+def b(f):
+    def new(*arg,**argw):
+        return f(*arg,**argw)
+    return new
+
+@b
+def a(c):
+    print 'a'
 
 if __name__ == '__main__':
-    print login('54080@42qu.com','794613852',u'10046352',u'1f082771f7c940849f6fd02ba5d3519f')
+    ret = login_by_email('54080@42qu.com','pw',u'10046352',u'1f082771f7c940849f6fd02ba5d3519f')
+    print ret.access_token
+    print ret.id
+    print user_info_get(ret.access_token,ret.id)
 
