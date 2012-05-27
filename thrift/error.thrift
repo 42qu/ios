04afd3e00e0f0478fdae9ba225087f48c8d5c199
张沈鹏 <zsp007@gmail.com>
2012-05-27 13:05:20 +0800

    fix

--- thrift/error.thrift
+++ thrift/error.thrift
@@ -1,34 +1,12 @@
-enum ErrorCode {
-  UNKNOWN = 1,
-  BAD_DATA_FORMAT = 2,
-  PERMISSION_DENIED = 3,
-  INTERNAL_ERROR = 4,
-  DATA_REQUIRED = 5,
-  LIMIT_REACHED = 6,
-  QUOTA_REACHED = 7,
-  INVALID_AUTH = 8,
-  AUTH_EXPIRED = 9,
-  DATA_CONFLICT = 10,
-  ENML_VALIDATION = 11,
-  SHARD_UNAVAILABLE = 12,
-  LEN_TOO_SHORT = 13,
-  LEN_TOO_LONG = 14,
-  TOO_FEW = 15,
-  TOO_MANY = 16,
-  UNSUPPORTED_OPERATION = 17
+enum ExceptionCode {
+  NOT_EXIST = 1,
+  NO_PRIVILEGE = 2,
 }
 
-exception UserException {
-  1:  required  ErrorCode errorCode,
-  2:  optional  string parameter
+
+exception Exception {
+  1:  optional  ExceptionCode code,
+  2:  optional  string message 
 }
 
-exception SystemException {
-  1:  required  ErrorCode errorCode,
-  2:  optional  string message
-}
 
-exception NotFoundException {
-  1:  optional  string identifier,
-  2:  optional  string key
-}
